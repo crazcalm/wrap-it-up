fd6ded52af32f75ca4e2a370f41205ded6d228f9
Marcus Willock <crazcalm@gmail.com>
2014-07-05 07:54:39 -0400

    filtering the my_subreddits response to give me only content

--- reddit_py/sort_reddits.py
+++ reddit_py/sort_reddits.py
@@ -3,6 +3,17 @@ __author__ = "crazcalm"
 from operator import itemgetter
 from itertools import groupby
 
+#    python_subs = [item["data"] for item in python_subs]
+
+def format_my_subreddits(response):
+    """
+    Returns a list of the content (in dictionary form) from the response.
+    """
+    tempt = []
+    for item in response:
+        tempt.append(item["data"])
+    return tempt
+
 def sort_by_key(items, key):
     """
     For a given list of dictionaries with consistant key namings, sort by the
