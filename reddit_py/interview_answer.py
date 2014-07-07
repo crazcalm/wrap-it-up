f4e4ce4148f74156ba2159f1ebb81c065ac8189e
Marcus Willock <crazcalm@gmail.com>
2014-07-07 07:51:31 -0400

    An example of how to use the api

---
+++ reddit_py/interview_answer.py
@@ -0,0 +1,25 @@
+import subreddits, user_class, sort_reddits
+from itertools import groupby
+from operator import itemgetter
+
+def main():
+
+    # loging in 
+    user = user_class.User("crazcalm", "3dKXw24Hv4")
+    client = user.login()
+
+    # consuming api
+    python_subs = subreddits.list_subreddit_submissions(client, "python", "hot")
+    python_subs = [item["data"] for item in python_subs]
+
+    # sorting
+    python_subs = sort_reddits.sort_by_key(python_subs, "author")
+
+    # printing out sorted list
+    for author, items in groupby(python_subs, key=itemgetter("author")):
+        print(author)
+        for i in items:
+            print("    ", i, "\n")
+
+if __name__ == "__main__":
+    main()
